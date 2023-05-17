e55c13e4472ad0dd9594ff36a14aac24c47202cf
nicugnm <nghergu17@gmail.com>
2023-05-17 17:15:17 +0300

    generics examples - complete

---
+++ lab12/src/main/java/org/example/Main.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class GenericClass<T> {
+
+    private T parameter;
+
+    public GenericClass(T parameter) {
+        this.parameter = parameter;
+    }
+
+    public void modifyObject(T parameter) {
+        ///
+    }
+}
+
+class NumberOperations<T extends Number> {
+
+    private T parameter;
+
+    public NumberOperations(T parameter) {
+        this.parameter = parameter;
+    }
+
+    public Double add(T number1, T number2) {
+        return number1.doubleValue() + number2.doubleValue();
+    }
+}
+
+class Animal {
+
+    private Integer age;
+
+    private String number;
+
+    public Animal(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
+class Dog extends Animal {
+
+    public Dog(Integer age) {
+        super(age);
+    }
+}
+
+class Cat extends Animal {
+
+    public Cat(Integer age) {
+        super(age);
+    }
+}
+
+class GenericAnimal<T extends Animal> {
+
+    private T genericAnimal;
+
+    // constructor
+    // metode
+}
